0f21b62082e191912c1420c3a4d6a4c0a4399c88
Monaal Sharma <monaalsharma162005@gmail.com>
2025-06-06 11:14:05 +0530

    Updated font styles to Mntserrat in index.css and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        // Original colors
         navy: {
           800: '#1E293B',
           900: '#0F172A',
@@ -15,10 +16,21 @@ export default {
           600: '#B08D6A',
           700: '#9C7852',
         },
+        'royal-blue': {
+          300: '#a1c4fd',
+          400: '#7193ff',
+          500: '#4169E1',
+          600: '#3956b1',
+          700: '#2e468b',
+          800: '#24366a',
+          900: '#1b2850',
+        },
+        deepRoyal: '#003C8C',
+        white: '#ffffff',
       },
       fontFamily: {
-        serif: ['Playfair Display', 'serif'],
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Montserrat', 'sans-serif'],
       },
       boxShadow: {
         'inner-lg': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
